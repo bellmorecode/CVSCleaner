6c573fb208dbbf0369c5585296a40ed97fa69159
Glenn Ferrie <glenn.ferrie@gmail.com>
2013-02-19 21:43:32 -0500

    first commit

---
+++ CVSCleaner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVSCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a path to clean: ");
+            var path = Console.ReadLine();
+
+            path = @"C:\Users\Glenn\Dropbox\CVS_ALL\CLIENTS\";
+
+            var folders = Directory.GetDirectories(path, "CVS", SearchOption.AllDirectories).Where(q => q.EndsWith("CVS"));
+            foreach (var folder in folders)
+            {
+                Console.WriteLine(folder);
+                Directory.Delete(folder, true);
+            }
+            Console.WriteLine("================================");
+            Console.WriteLine("Count: {0}", folders.Count());
+            Console.WriteLine("================================");
+            var final = Console.ReadLine();
+        }
+    }
+}
